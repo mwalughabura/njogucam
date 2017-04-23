2900fca4519ebabf46d70ebf4b5180be6267d33c
mwalughabura <mwalughabura@gmail.com>
2017-04-23 09:09:31 +0300

    added the camera plugin

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,26 +1,10 @@
-import {Page} from 'ionic-angular';
-import {Camera} from 'ionic-native';
+import { Diagnostic } from 'ionic-native';
 
-@Page({
-  templateUrl: 'build/pages/home/home.html'
-})
-export class HomePage {
-  public base64Image: string;
-
-  constructor() {
-
-  }
-
-  takePicture(){
-    Camera.getPicture({
-        destinationType: Camera.DestinationType.DATA_URL,
-        targetWidth: 1000,
-        targetHeight: 1000
-    }).then((imageData) => {
-      // imageData is a base64 encoded string
-        this.base64Image = "data:image/jpeg;base64," + imageData;
-    }, (err) => {
-        console.log(err);
-    });
-  }
+constructor(public navCtrl: NavController, 
+            public toastCtrl: ToastController) {
+    this.checkPermissions();
+}
+ 
+checkPermissions() {
+    // More code goes here
 }
